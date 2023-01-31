4a21212baedf11ea2779745a7abd4c82bfc839b4
kolserdav <serega12101983@gmail.com>
2023-02-01 03:56:19 +0700

    split line

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,36 @@ struct Latins<'a> {
     value: u8,
 }
 
+fn main() {
+    let latins = create_latins();
+    loop {
+        let stdin = std::io::stdin().lines();
+        let mut line: Option<String> = None;
+        for l in stdin {
+            line = Some(l.unwrap());
+            break;
+        }
+        if let Some(v) = line {
+            let line_i = v.split(" ");
+            let vec: Vec<&str> = line_i.collect();
+            for i in &vec {
+                println!("{:?}", i);
+            }
+        } else {
+            println!("Line not set");
+        }
+    }
+    /*
+    for l in &latins {
+        println!("{:?}", l.name);
+    }
+    */
+}
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
 fn create_latins<'b>() -> Vec<Latins<'b>> {
     let mut latins = Vec::new();
     latins.push(Latins {
@@ -47,11 +77,4 @@ fn create_latins<'b>() -> Vec<Latins<'b>> {
         value: 10,
     });
     latins
-}
-
-fn main() {
-    let latins = create_latins();
-    for l in &latins {
-        println!("{:?}", l.name);
-    }
 }
